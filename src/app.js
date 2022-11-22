e98a9f8946d0d34d6d8f93a09dd35be58d01a218
htx <huangtingxin@rayvison.com>
2022-11-22 17:06:28 +0800

    🎉 initial tiny projects

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const fileUpload = require('express-fileupload')
+const app = express()
+
+app.use(
+  fileUpload({
+    limits: {
+      fileSize: 10 * 1024 * 1024,
+    },
+    abortOnLimit: true,
+  })
+)
+
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  next()
+})
+
+app.post('/tinymce-contenful/image-upload', (req, res) => {
+  try {
+    const { image } = req.files
+    if (!image) return res.sendStatus(400)
+    const fileName = `${image.md5}_${image.name}`
+    image.mv(__dirname + '/images/' + fileName)
+    const location = `https://www.raysync.io/tinymce-contentful/images/${fileName}`
+    res.status(200).send({ location })
+  } catch (e) {
+    console.log(e)
+    res.status(500).send({ e: JSON.stringify(e) })
+  }
+})
+
+app.listen(300, () => {
+  console.log(`Example app listening on port ${port}`)
+})
--- src/tinymce-for-contentful.js
+++ src/tinymce-for-contentful.js
@@ -1,95 +1,143 @@
-window.contentfulExtension.init(function(api) {
-  function tinymceForContentful(api) {
-    function tweak(param) {
-      var t = param.trim();
-      if (t === "false") {
-        return false;
-      } else if (t === "") {
-        return undefined;
-      } else {
-        return t;
-      }
+const images_upload_handler = (blobInfo, progress) =>
+  new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest()
+    xhr.withCredentials = false
+    xhr.open('POST', 'https://www.raysync.io/tinymce-contenful/image-upload')
+
+    xhr.upload.onprogress = (e) => {
+      progress((e.loaded / e.total) * 100)
     }
+    xhr.onload = () => {
+      if (xhr.status === 403) {
+        reject({ message: 'HTTP Error: ' + xhr.status, remove: true })
+        return
+      }
+      if (xhr.status < 200 || xhr.status >= 300) {
+        reject('HTTP Error: ' + xhr.status + ',' + xhr.responseText)
+        return
+      }
+      const json = JSON.parse(xhr.responseText)
+      if (!json || typeof json.location != 'string') {
+        reject('Invalid JSON: ' + xhr.responseText)
+        return
+      }
+      resolve(json.location)
+    }
+    xhr.onerror = () => {
+      reject('Image upload failed due to a XHR Transport error. Code: ' + xhr.status)
+    }
+    const formData = new FormData()
+    formData.append('image', blobInfo.blob(), blobInfo.filename())
 
-    var p = tweak(api.parameters.instance.plugins);
-    var tb = tweak(api.parameters.instance.toolbar);
-    var mb = tweak(api.parameters.instance.menubar);  
-
-    api.window.startAutoResizer();
-
+    xhr.send(formData)
+  })
+window.contentfulExtension.init(function (api) {
+  function tinymceForContentful(api) {
+    api.window.startAutoResizer()
     tinymce.init({
-      selector: "#editor",
-      plugins: p,
-      toolbar: tb,
-      menubar: mb,
+      selector: '#editor',
+      language: 'zh-Hans',
+      language_url: './zh-Hans.js',
+      plugins: [
+        'advlist',
+        'autolink',
+        'lists',
+        'link',
+        'image',
+        'charmap',
+        'anchor',
+        'searchreplace',
+        'visualblocks',
+        'code',
+        'fullscreen',
+        'insertdatetime',
+        'media',
+        'table',
+        'preview',
+        'help',
+        'wordcount',
+      ],
+      toolbar1:
+        'undo redo | fullscreen ｜ link image media | blockquote backcolor forecolor underline strikethrough bold italic forecolor removeformat | ',
+      toolbar2:
+        'bullist numlist | outdent indent | alignleft aligncenter alignright alignjustify | ',
+      toolbar3: 'lineheight | fontfamily fontsize blocks styles',
+      link_rel_list: [
+        { title: 'No Referrer', value: 'noreferrer' },
+        { title: 'External Link', value: 'external' },
+        { title: 'nofollow', value: 'nofollow' },
+      ],
+      link_default_target: '_blank',
+      fullscreen_native: !0,
+      content_style:
+        'body { font-family:Helvetica,Arial,sans-serif; font-size:14px } img { max-width: 90%; }',
       min_height: 600,
       max_height: 750,
       autoresize_bottom_margin: 15,
       resize: false,
       image_caption: true,
-      init_instance_callback : function(editor) {
-        var listening = true;
+      images_upload_handler: images_upload_handler,
+      init_instance_callback: function (editor) {
+        var listening = true
 
         function getEditorContent() {
-          return editor.getContent() || '';
+          return editor.getContent() || ''
         }
 
         function getApiContent() {
-          return api.field.getValue() || '';
+          return api.field.getValue() || ''
         }
 
         function setContent(x) {
-          var apiContent = x || '';
-          var editorContent = getEditorContent();
+          var apiContent = x || ''
+          var editorContent = getEditorContent()
           if (apiContent !== editorContent) {
             //console.log('Setting editor content to: [' + apiContent + ']');
-            editor.setContent(apiContent);
+            editor.setContent(apiContent)
           }
         }
 
-        setContent(api.field.getValue());
+        setContent(api.field.getValue())
 
-        api.field.onValueChanged(function(x) {
+        api.field.onValueChanged(function (x) {
           if (listening) {
-            setContent(x);
+            setContent(x)
           }
-        });
+        })
 
         function onEditorChange() {
-          var editorContent = getEditorContent();
-          var apiContent = getApiContent();
+          var editorContent = getEditorContent()
+          var apiContent = getApiContent()
 
           if (editorContent !== apiContent) {
             //console.log('Setting content in api to: [' + editorContent + ']');
-            listening = false;
-            api.field.setValue(editorContent).then(function() {
-              listening = true;
-            }).catch(function(err) {
-              console.log("Error setting content", err);
-              listening = true;
-            });
+            listening = false
+            api.field
+              .setValue(editorContent)
+              .then(function () {
+                listening = true
+              })
+              .catch(function (err) {
+                console.log('Error setting content', err)
+                listening = true
+              })
           }
         }
 
-        var throttled = _.throttle(onEditorChange, 500, {leading: true});
-        editor.on('change keyup setcontent blur', throttled);
-      }
-    });
+        var throttled = _.throttle(onEditorChange, 500, { leading: true })
+        editor.on('change keyup setcontent blur', throttled)
+      },
+    })
   }
 
   function loadScript(src, onload) {
-    var script = document.createElement('script');
-    script.setAttribute('src', src);
-    script.onload = onload;
-    document.body.appendChild(script);
+    var script = document.createElement('script')
+    script.setAttribute('src', src)
+    script.onload = onload
+    document.body.appendChild(script)
   }
 
-  var sub = location.host == "contentful.staging.tiny.cloud" ? "cdn.staging" : "cdn";
-  var apiKey = api.parameters.installation.apiKey;
-  var channel = api.parameters.installation.channel;
-  var tinymceUrl = "https://" + sub + ".tiny.cloud/1/" + apiKey + "/tinymce/" + channel + "/tinymce.min.js";
-
-  loadScript(tinymceUrl, function() {
-    tinymceForContentful(api);
-  });
-});
+  loadScript('./tinymce.min.js', function () {
+    tinymceForContentful(api)
+  })
+})
